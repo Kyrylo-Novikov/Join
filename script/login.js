16ecc287e62fe7424441530d17c56757bdba6cf5
Kyrylo Novikov <novikovkyrylo@gmail.com>
2025-12-28 11:50:07 +0100

    fix: correct links so project loads properly

--- script/login.js
+++ script/login.js
@@ -1,15 +1,13 @@
 loggedin = false;
 
-
 /**
  * init runs all function which are aquired to run onload of the html
  */
 function init() {
-  startAnimation()
-  pushToUsersArray()
-  deleteLocalUser()
+  startAnimation();
+  pushToUsersArray();
+  deleteLocalUser();
 }
-
 
 /**
  * Starts the Animation for the Login page Join Logo
@@ -18,28 +16,27 @@ function startAnimation() {
   const logo = document.getElementById("animated_logo");
 
   setTimeout(() => {
-      logo.classList.add("join_logo");
-      logo.classList.remove("join_logo_start")
-      }, 100);
+    logo.classList.add("join_logo");
+    logo.classList.remove("join_logo_start");
+  }, 100);
 }
 
-
 /**
- * checks if the inputs in the form are valid and displays error messages if they are not 
+ * checks if the inputs in the form are valid and displays error messages if they are not
  */
 function validateLoginForm() {
-    let email = document.getElementById("email");
-    let password = document.getElementById("password");
-    let errorMsg = document.getElementById("error_msg");
-    if (!email.validity.valid || !password.validity.valid) {
-      displayError()
-      return false;
-    } else {
-        email.classList.remove("error"); 
-        password.classList.remove("error");
-        errorMsg.style.display = "none";
-        return true;
-      }
+  let email = document.getElementById("email");
+  let password = document.getElementById("password");
+  let errorMsg = document.getElementById("error_msg");
+  if (!email.validity.valid || !password.validity.valid) {
+    displayError();
+    return false;
+  } else {
+    email.classList.remove("error");
+    password.classList.remove("error");
+    errorMsg.style.display = "none";
+    return true;
+  }
 }
 
 /**
@@ -47,9 +44,8 @@ function validateLoginForm() {
  */
 function redirectToSummary() {
   localStorage.setItem("isLoggedIn", "true");
-  window.location.href = "/html/summary.html";
+  window.location.href = "html/summary.html";
 }
-
 
 /**
  * checks if the users e-mail and password match to login
@@ -58,18 +54,17 @@ function login() {
   if (!validateLoginForm()) return;
   const emailInput = document.getElementById("email").value;
   const passwordInput = document.getElementById("password").value;
-  const user = users.find(user => user.userData.email === emailInput);
+  const user = users.find((user) => user.userData.email === emailInput);
   if (user) {
-      if (user.userData.password === passwordInput) {
-          handleSuccessfulLogin(user.userData);
-      } else {
-        displayError()
-      }
+    if (user.userData.password === passwordInput) {
+      handleSuccessfulLogin(user.userData);
+    } else {
+      displayError();
+    }
   } else {
-    displayError()
+    displayError();
   }
 }
-
 
 /**
  * Displays the errors for wrong inputs like email or password
@@ -78,28 +73,25 @@ function displayError() {
   let email = document.getElementById("email");
   let password = document.getElementById("password");
   let errorMsg = document.getElementById("error_msg");
-  email.classList.add("error"); 
-  password.classList.add("error"); 
+  email.classList.add("error");
+  password.classList.add("error");
   errorMsg.style.display = "block";
 }
-
 
 /**
  * when login is succesful redirect the user to the summary
  * @param {*} userData - userdata to give over information as parameter
  */
 function handleSuccessfulLogin(userData) {
-  localStorage.setItem('user', JSON.stringify(userData));
+  localStorage.setItem("user", JSON.stringify(userData));
   console.log(userData);
-  redirectToSummary()
+  redirectToSummary();
 }
 
 /**
- * clears the localStorage if the user was already logged in and loged out 
+ * clears the localStorage if the user was already logged in and loged out
  */
 function deleteLocalUser() {
-  localStorage.removeItem('user');
-  localStorage.removeItem('isLoggedIn');
+  localStorage.removeItem("user");
+  localStorage.removeItem("isLoggedIn");
 }
-
-
